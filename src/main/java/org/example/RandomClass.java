e6facb8a968a5c2781fe7ef0f5e2e044ac294865
charlesg112 <charles.antoine.giroux42@gmail.com>
2022-09-21 11:44:42 -0400

    test checkstyle error

--- src/main/java/org/example/RandomClass.java
+++ src/main/java/org/example/RandomClass.java
@@ -1,7 +1,7 @@
 package org.example;
 
 public class RandomClass {
-    private float testCase = 2;
+    private float testCase___LMAO = 2;
     public int Get___Number() {
         float test = 2;
         return 3;
