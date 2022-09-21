e0d6382db84642ae29fbecd67942867ef3ccefd5
charlesg112 <charles.antoine.giroux42@gmail.com>
2022-09-21 11:10:28 -0400

    test

---
+++ src/main/java/org/example/RandomClass.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class RandomClass {
+    public int GetNumber() {
+        return 3;
+    }
+}
---
+++ src/test/java/org/example/RandomClassTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class RandomClassTest {
+    @Test
+    public void shouldReturn3() {
+        RandomClass random = new RandomClass();
+
+        assertEquals(random.GetNumber(), 3);
+    }
+}
